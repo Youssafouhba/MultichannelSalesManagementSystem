392bc83f7ff3d8dfa6332a5b6c7b1a6216ea5902
3lacker <160363469+3lacker@users.noreply.github.com>
2024-08-04 15:48:25 +0000

    Add files via upload

---
+++ DiscoveryService-for render/src/test/java/com/example/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_server;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.wholesaled.eureka_server.EurekaServerApplication;
+
+@SpringBootTest(classes = EurekaServerApplication.class)
+public class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
